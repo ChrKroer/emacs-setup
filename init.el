69121287fbfc38136a28f14586205003cc641a84
ChrKroer <chrkroer@gmail.com>
2015-02-26 17:15:02 -0500

    added modularization of initialization files

--- init.el
+++ init.el
@@ -1,5 +1,4 @@
-(setq c-default-style "linux"
-          c-basic-offset 4)
+
 
 (defun iwb ()
   "indent whole buffer"
@@ -8,38 +7,35 @@
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max)))
 
-;(add-to-list 'load-path "/Users/ckroer/.emacs.d/")
-;(require 'julia-mode)
-
-(tool-bar-mode 0)
-;(tabbar-mode 0)
-;(c-set-offset 'innamespace 0)
-
-(setq py-split-windows-on-execute-p nil)
-
-(auto-fill-mode -1)
-(remove-hook 'text-mode-hook #'turn-on-auto-fill)
-(add-hook 'LaTeX-mode-hook '(flyspell-mode t))
 
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
+;;(auto-fill-mode -1)
+;;(remove-hook 'text-mode-hook #'turn-on-auto-fill)
 
-(load-theme 'tango-dark t)
+;; code that enables modularization below
+(defconst user-init-dir
+  (cond ((boundp 'user-emacs-directory)
+         user-emacs-directory)
+        ((boundp 'user-init-directory)
+         user-init-directory)
+        (t "~/.emacs.d/")))
 
-(require 'package) ;; You might already have this line
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
+
+(defun load-user-file (file)
+  (interactive "f")
+  "Load a file in current user's configuration directory"
+  (load-file (expand-file-name file user-init-dir)))
+
+
+;; load package stuff first, as it sets certain initializations needed for below .el files
+(load-user-file "package.el")
+
+
+(load-user-file "ui.el")
+(load-user-file "tex.el")
+(load-user-file "el-get.el")
+(load-user-file "c-code.el")
+
+
