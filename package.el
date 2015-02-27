4872e51eb5979fc1cf72d4a7bf8eb8cbe879db33
ChrKroer <chrkroer@gmail.com>
2015-02-27 13:20:00 -0500

    updated auto-install to not use prelude

--- package.el
+++ package.el
@@ -50,31 +50,7 @@
 
 (install-packages)
 
-;; (defvar prelude-packages
-;;   '(               ac-clang
-;; 		   auctex
-;; 		   auctex-latexmk
-;; 		   auto-complete
-;; 		   company
-;; 		   ecb
-;; 		   solarized-theme
-;; 		   )
-;;   "A list of packages to ensure are installed at launch.")
 
-;; (defun prelude-packages-installed-p ()
-;;   (loop for p in prelude-packages
-;;         when (not (package-installed-p p)) do (return nil)
-;;         finally (return t)))
-
-;; (unless (prelude-packages-installed-p)
-;;   ;; check for new packages (package versions)
-;;   (message "%s" "Emacs Prelude is now refreshing its package database...")
-;;   (package-refresh-contents)
-;;   (message "%s" " done.")
-;;   ;; install the missing packages
-;;   (dolist (p prelude-packages)
-;;     (when (not (package-installed-p p))
-;;       (package-install p))))
 
 (provide 'prelude-packages)
 ;;; prelude-packages.el ends here
