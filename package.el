7629443e552610d50041633e9f05dc8bac06fe01
ChrKroer <chrkroer@gmail.com>
2015-02-27 13:17:26 -0500

    updated auto-install to not use prelude

--- package.el
+++ package.el
@@ -8,31 +8,73 @@
 
 (require 'cl)
 
-(defvar prelude-packages
-  '(               ac-clang
-		   auctex
-		   auctex-latexmk
-		   auto-complete
-		   company
-		   ecb
-		   solarized-theme
-		   )
-  "A list of packages to ensure are installed at launch.")
+(defconst used-packages
+   '(               
+     ac-clang
+     auctex
+     auctex-latexmk
+     auto-complete
+     company
+     ecb
+     solarized-theme
+     ))
+  ;; '(anzu
+  ;;   company
+  ;;   duplicate-thing
+  ;;   ggtags
+  ;;   helm
+  ;;   helm-gtags
+  ;;   helm-projectile
+  ;;   helm-swoop
+  ;;   function-args
+  ;;   clean-aindent-mode
+  ;;   comment-dwim-2
+  ;;   dtrt-indent
+  ;;   ws-butler
+  ;;   iedit
+  ;;   yasnippet
+  ;;   smartparens
+  ;;   projectile
+  ;;   volatile-highlights
+  ;;   undo-tree
+  ;;   zygospore))
 
-(defun prelude-packages-installed-p ()
-  (loop for p in prelude-packages
-        when (not (package-installed-p p)) do (return nil)
-        finally (return t)))
+(defun install-packages ()
+  "Install all required packages."
+  (interactive)
+  (unless package-archive-contents
+    (package-refresh-contents))
+  (dolist (package used-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
 
-(unless (prelude-packages-installed-p)
-  ;; check for new packages (package versions)
-  (message "%s" "Emacs Prelude is now refreshing its package database...")
-  (package-refresh-contents)
-  (message "%s" " done.")
-  ;; install the missing packages
-  (dolist (p prelude-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+(install-packages)
+
+;; (defvar prelude-packages
+;;   '(               ac-clang
+;; 		   auctex
+;; 		   auctex-latexmk
+;; 		   auto-complete
+;; 		   company
+;; 		   ecb
+;; 		   solarized-theme
+;; 		   )
+;;   "A list of packages to ensure are installed at launch.")
+
+;; (defun prelude-packages-installed-p ()
+;;   (loop for p in prelude-packages
+;;         when (not (package-installed-p p)) do (return nil)
+;;         finally (return t)))
+
+;; (unless (prelude-packages-installed-p)
+;;   ;; check for new packages (package versions)
+;;   (message "%s" "Emacs Prelude is now refreshing its package database...")
+;;   (package-refresh-contents)
+;;   (message "%s" " done.")
+;;   ;; install the missing packages
+;;   (dolist (p prelude-packages)
+;;     (when (not (package-installed-p p))
+;;       (package-install p))))
 
 (provide 'prelude-packages)
 ;;; prelude-packages.el ends here
