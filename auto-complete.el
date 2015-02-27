382d1bda1fa29f144f781f56e114b3a164737abd
ChrKroer <chrkroer@gmail.com>
2015-02-27 11:11:43 -0500

    added auto-install

---
+++ auto-complete.el
@@ -0,0 +1,6 @@
+(require 'auto-complete)
+
+(global-auto-complete-mode t)
+
+; change the below to do custom ac-modes
+;(setq ac-modes '(c++-mode sql-mode))
--- package.el
+++ package.el
@@ -5,3 +5,34 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
+
+(require 'cl)
+
+(defvar prelude-packages
+  '(               ac-clang
+		   auctex
+		   auctex-latexmk
+		   auto-complete
+		   company
+		   ecb
+		   solarized-theme
+		   )
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'prelude-packages)
+;;; prelude-packages.el ends here
